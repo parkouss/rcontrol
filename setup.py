dea7b59230a516f07a174f12692dcb4711abae0f
Julien Pagès <j.parkouss@gmail.com>
2015-06-13 22:12:17 +0200

    add LGPL licence information

--- setup.py
+++ setup.py
@@ -1,5 +1,20 @@
 # -*- coding: utf-8 -*-
 
+# This file is part of rcontrol.
+#
+# rcontrol is free software; you can redistribute it and/or modify it under the
+# terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation; either version 3 of the License, or (at your option)
+# any later version.
+#
+# rcontrol is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+# details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with rcontrol. If not, see <http://www.gnu.org/licenses/>.
+
 from setuptools import setup
 import os
 import re
@@ -27,7 +42,7 @@ setup(
     author_email="j.parkouss@gmail.com",
     install_requires=['six', 'paramiko'],
     url='http://github.com/parkouss/rcontrol',
-    license="GPL 2.0/LGPL 2.1",
+    license="LGPL",
     packages=['rcontrol'],
     platforms=['Any'],
     tests_require=['mock'],

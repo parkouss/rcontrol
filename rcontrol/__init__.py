f4b61dc11b4ba2f745be2f4fe63a59eca27d27da
Julien Pagès <j.parkouss@gmail.com>
2015-06-15 21:09:13 +0200

    bump release to 0.1.2

--- rcontrol/__init__.py
+++ rcontrol/__init__.py
@@ -13,4 +13,4 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with rcontrol. If not, see <http://www.gnu.org/licenses/>.
 
-__version__ = '0.1.1'
+__version__ = '0.1.2'
